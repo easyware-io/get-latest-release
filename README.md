d8acca0ed8148efb3f6c362a923a44fe74da55fc
Danilo Koerber <danilo.koerber@bmwgroup.com>
2024-06-24 11:32:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,160 @@
+# easyware-io/get-latest-release@v1
+
+![CI](https://github.com/easyware-io/get-latest-release/actions/workflows/build.yml/badge.svg)
+
+Get latest release information from a GitHub repository
+
+## Usage
+
+### Basic usage
+
+```yaml
+- name: Get latest release
+  uses: easyware-io/get-latest-release@v1
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Inputs
+
+#### `token`
+
+GitHub token
+
+#### `owner`
+
+Optional. Owner of the repository. Defaults to current owner.
+
+#### `repo`
+
+Optional. Name of the repository. Defaults to current repo.
+
+#### `excludes`
+
+Optional. Comma separated list of releases types to exclude. Valid values are: "draft", "prerelease"
+
+### Outputs
+
+#### `id`
+
+ID of the release
+
+#### `name`
+
+Name of the release
+
+#### `tag_name`
+
+Tag name of the release
+
+#### `body`
+
+Body of the release
+
+#### `draft`
+
+Whether the release is a draft
+
+#### `prerelease`
+
+Whether the release is a prerelease
+
+#### `created_at`
+
+Creation date of the release
+
+#### `published_at`
+
+Publication date of the release
+
+#### `url`
+
+URL of the release
+
+#### `html_url`
+
+HTML URL of the release
+
+#### `assets_url`
+
+URL of the release assets
+
+#### `upload_url`
+
+URL for uploading assets to the release
+
+#### `tarball_url`
+
+URL of the release tarball
+
+#### `zipball_url`
+
+URL of the release zipball
+
+#### `node_id`
+
+Node ID of the release
+
+#### `author_login`
+
+Login of the release author
+
+#### `author_id`
+
+ID of the release author
+
+#### `author_node_id`
+
+Node ID of the release author
+
+#### `author_avatar_url`
+
+Avatar URL of the release author
+
+#### `author_html_url`
+
+HTML URL of the release author
+
+#### `author_followers_url`
+
+URL of the followers of the release author
+
+#### `author_following_url`
+
+URL of the users followed by the release author
+
+#### `author_gists_url`
+
+URL of the gists created by the release author
+
+#### `author_starred_url`
+
+URL of the repositories starred by the release author
+
+#### `author_subscriptions_url`
+
+URL of the subscriptions of the release author
+
+#### `author_organizations_url`
+
+URL of the organizations the release author belongs to
+
+#### `author_repos_url`
+
+URL of the repositories owned by the release author
+
+#### `author_events_url`
+
+URL of the events performed by the release author
+
+#### `author_received_events_url`
+
+URL of the events received by the release author
+
+#### `author_type`
+
+Type of the release author
+
+#### `author_site_admin`
+
+Whether the release author is a site admin
---
+++ src/main.ts
@@ -0,0 +1,112 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+export default async function run(): Promise<void> {
+  try {
+    core.debug('Getting owner and repo from context');
+    const currentOwner = github.context.repo.owner;
+    const currentRepo = github.context.repo.repo;
+
+    core.debug('Getting inputs from the user');
+    const token = core.getInput('token', { required: true });
+    const owner = core.getInput('owner') || currentOwner;
+    const repo = core.getInput('repo') || currentRepo;
+    const excludes: string[] = core.getInput('excludes').trim().toLowerCase().split(',') || [];
+
+    core.debug(`Creating Octokit instance with token: ${token}`);
+    const octokit = github.getOctokit(token);
+
+    const releasesRaw = await octokit.repos.listReleases({
+      owner,
+      repo,
+    });
+    let releases = releasesRaw.data;
+
+    core.info(`Found ${releases.length} releases`);
+    core.info(`Excludes: ${excludes}`);
+    core.info(`Releases: ${JSON.stringify(releases)}`);
+
+    if (excludes.includes('prerelease')) {
+      releases = releases.filter((x: { prerelease: boolean }) => x.prerelease !== true);
+    }
+    if (excludes.includes('draft')) {
+      releases = releases.filter((x: { draft: boolean }) => x.draft !== true);
+    }
+
+    let latestRelease = null;
+    if (releases.length === 0) {
+      core.error(`No release found.`);
+      core.setFailed(`No release found.`);
+    } else {
+      latestRelease = releases[0];
+    }
+
+    core.debug(`Setting outputs`);
+    core.setOutput('id', latestRelease.id);
+    core.setOutput('name', latestRelease.name);
+    core.setOutput('tag_name', latestRelease.tag_name);
+    core.setOutput('body', latestRelease.body);
+    core.setOutput('draft', latestRelease.draft);
+    core.setOutput('prerelease', latestRelease.prerelease);
+    core.setOutput('created_at', latestRelease.created_at);
+    core.setOutput('published_at', latestRelease.published_at);
+    core.setOutput('url', latestRelease.url);
+    core.setOutput('html_url', latestRelease.html_url);
+    core.setOutput('assets_url', latestRelease.assets_url);
+    core.setOutput('upload_url', latestRelease.upload_url);
+    core.setOutput('tarball_url', latestRelease.tarball_url);
+    core.setOutput('zipball_url', latestRelease.zipball_url);
+    core.setOutput('node_id', latestRelease.node_id);
+    core.setOutput('author_login', latestRelease.author.login);
+    core.setOutput('author_id', latestRelease.author.id);
+    core.setOutput('author_node_id', latestRelease.author.node_id);
+    core.setOutput('author_avatar_url', latestRelease.author.avatar_url);
+    core.setOutput('author_html_url', latestRelease.author.html_url);
+    core.setOutput('author_followers_url', latestRelease.author.followers_url);
+    core.setOutput('author_following_url', latestRelease.author.following_url);
+    core.setOutput('author_gists_url', latestRelease.author.gists_url);
+    core.setOutput('author_starred_url', latestRelease.author.starred_url);
+    core.setOutput('author_subscriptions_url', latestRelease.author.subscriptions_url);
+    core.setOutput('author_organizations_url', latestRelease.author.organizations_url);
+    core.setOutput('author_repos_url', latestRelease.author.repos_url);
+    core.setOutput('author_events_url', latestRelease.author.events_url);
+    core.setOutput('author_received_events_url', latestRelease.author.received_events_url);
+    core.setOutput('author_type', latestRelease.author.type);
+    core.setOutput('author_site_admin', latestRelease.author.site_admin);
+  } catch (error) {
+    core.setOutput('id', null);
+    core.setOutput('name', null);
+    core.setOutput('tag_name', null);
+    core.setOutput('body', null);
+    core.setOutput('draft', null);
+    core.setOutput('prerelease', null);
+    core.setOutput('created_at', null);
+    core.setOutput('published_at', null);
+    core.setOutput('url', null);
+    core.setOutput('html_url', null);
+    core.setOutput('assets_url', null);
+    core.setOutput('upload_url', null);
+    core.setOutput('tarball_url', null);
+    core.setOutput('zipball_url', null);
+    core.setOutput('node_id', null);
+    core.setOutput('author_login', null);
+    core.setOutput('author_id', null);
+    core.setOutput('author_node_id', null);
+    core.setOutput('author_avatar_url', null);
+    core.setOutput('author_html_url', null);
+    core.setOutput('author_followers_url', null);
+    core.setOutput('author_following_url', null);
+    core.setOutput('author_gists_url', null);
+    core.setOutput('author_starred_url', null);
+    core.setOutput('author_subscriptions_url', null);
+    core.setOutput('author_organizations_url', null);
+    core.setOutput('author_repos_url', null);
+    core.setOutput('author_events_url', null);
+    core.setOutput('author_received_events_url', null);
+    core.setOutput('author_type', null);
+    core.setOutput('author_site_admin', null);
+    core.info(`Release not found.`);
+  }
+}
+
+run();
